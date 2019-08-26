443d939363072f451fd5306272890634b912f611
D.Alijerban <D.Alijerban@ALIJERBAN-PC>
2019-08-26 16:36:12 +0430

    Add _Layout

---
+++ HMS.Web/Areas/Admin/Controllers/TestAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HMS.Web.Areas.Admin.Controllers
+{
+    public class TestAdminController : Controller
+    {
+        // GET: Admin/TestAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
