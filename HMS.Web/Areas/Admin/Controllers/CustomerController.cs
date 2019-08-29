fcebd8be50548f1ec78a6985d119c58a0101ffc6
D.Alijerban <D.Alijerban@ALIJERBAN-PC>
2019-08-29 12:28:03 +0430

    Add Read Action of the customer

---
+++ HMS.Web/Areas/Admin/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using HMS.Model.Core.DTOs.Customer;
+using HMS.Web.Controllers.Base;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace HMS.Web.Areas.Admin.Controllers
+{
+    public class CustomerController : BaseController
+    {
+        // GET: Admin/Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Customer_Read()
+        {
+            var customers = uow.Customer.GetAll();
+            var customerDtos = new List<GetCustomerDto>();
+
+            foreach (var customer in customers)
+            {
+                customerDtos.Add(new GetCustomerDto()
+                {
+                    Id = customer.Id,
+                    FirstName = customer.FirstName,
+                    LastName = customer.LastName,
+                    PassportNo = customer.PassportNo,
+                    NationalNo = customer.NationalNo
+                });
+            }
+
+            return Json(customerDtos, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
